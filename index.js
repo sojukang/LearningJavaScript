f6c7d66f9be45a0ba02d5a57e76f44fb3f8b8e2a
sojukang <tmdwn_1112@naver.com>
2021-07-22 17:41:45 +0900

    Create calcWage.js

--- index.js
+++ index.js
@@ -61,4 +61,12 @@ function introduce(name, birth, nationality, job) {
 
 console.log('introduce test');
 introduce('붕붕이', '98.11.16', 'S. Korea', 'Student');
-console.log('Test end')
+console.log('Test end');
+
+// String ex
+console.log(`한국 영화 역사상 아카데미상을 받은 것은 '기생충'이 처음이다.`);
+console.log(`아리스토텔레스는 "인간은 사회적 동물이다."라고 말했다.`);
+
+console.log(`영화 '베테랑'에서 "어이가 없네~"라는 대사가 유명했다.`);
+console.log('영화 \'베테랑\'에서 "어이가 없네~"라는 대사가 유명했다.');
+
