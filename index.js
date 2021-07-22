b992add33e0103c1283d21a610e289bb450c2d61
sojukang <tmdwn_1112@naver.com>
2021-07-22 13:50:51 +0900

    Add introduce to index.js

--- index.js
+++ index.js
@@ -50,3 +50,15 @@ console.log('printSum function test')
 printSum(10, 5);
 console.log(' ')
 
+function introduce(name, birth, nationality, job) {
+    console.log('안녕하세요! 반갑습니다.');
+    console.log('제 이름은 ' + name + ' 입니다.');
+    console.log('생년월일은 ' + birth + ' 이고');
+    console.log('국적은 ' + nationality + ' 입니다.');
+    console.log('직업은 ' + job + ' 입니다.');
+    console.log('잘 부탁드립니다!');
+};
+
+console.log('introduce test');
+introduce('붕붕이', '98.11.16', 'S. Korea', 'Student');
+console.log('Test end')
