bf9ec820552cc97a41f912505089390b24ba5b6e
sojukang <tmdwn_1112@naver.com>
2021-07-23 00:59:13 +0900

    Add printGrade in control.js

--- control.js
+++ control.js
@@ -1,9 +1,13 @@
 // checkHeight function for learning if statement
 function checkHeight(height) {
-    if (height >= 140) {
-        console.log('탑승이 가능합니다.');
+    const LIMIT = 140;
+    let passMessage = '탑승이 가능합니다.';
+    let failMessage = '탑승이 불가능합니다.';
+
+    if (height >= LIMIT) {
+        console.log(passMessage);
     } else {
-        console.log('탑승이 불가능합니다.');
+        console.log(failMessage);
     };
 };
 
@@ -11,3 +15,26 @@ function checkHeight(height) {
 checkHeight(141);
 checkHeight(135);
 checkHeight(170);
+
+// printGrade function for learning else if statement
+function printGrade(midtermScore, finalScore) {
+    let totalScore = midtermScore + finalScore;
+
+    if (totalScore >= 90) {
+        console.log('A');
+    } else if (totalScore >= 80) {
+        console.log('B');
+    } else if (totalScore >= 70) {
+        console.log('C');
+    } else if (totalScore >= 60) {
+        console.log('D');
+    } else {
+        console.log('F');
+    }
+};
+
+// 테스트 코드
+printGrade(25, 35);
+printGrade(50, 45);
+printGrade(29, 24);
+printGrade(37, 42);
