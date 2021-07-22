f1980c7a67e45aa3f671973f11cd39ddea052cc9
sojukang <tmdwn_1112@naver.com>
2021-07-22 13:25:34 +0900

    Create LearningJaveScript repository

---
+++ calorieCalculator.js
@@ -0,0 +1,14 @@
+// let variables 
+let espresso = 10;
+let milk = 170;
+let chocolateSyrup = 50;
+let whippedCream = 60;
+
+// test for each menu
+console.log(espresso); // 에스프레소 칼로리
+console.log(espresso + milk); // 라떼 칼로리
+console.log(espresso + chocolateSyrup + milk); // 모카 칼로리
+console.log(espresso + chocolateSyrup + milk + whippedCream); // 모카(휘핑 추가) 칼로리d
+
+
+
---
+++ index.js
@@ -0,0 +1,42 @@
+console.log('Hello World!');
+console.log(15);
+console.log(10 + 5);
+console.log(3 * 5);
+console.log(30 / 2);
+//console.log(3 * (4 + 1));
+
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let machaPrice = 4800;
+
+console.log(espressoPrice);
+console.log(lattePrice * 4 + machaPrice);
+console.log(" ")
+
+// declare function
+function greetings(sentence) {
+    console.log('Hi');
+    console.log('안녕');
+    console.log('Guten tag');
+    console.log(sentence);
+};
+
+// call function
+greetings("Hola");
+
+
+function welcome(name) {
+    console.log('안녕하세요 ' + name + '님!');
+};
+
+welcome('붕붕이');
+welcome('씅주캉');
+
+function printSquare(x) {
+    console.log(x * x);
+};
+
+console.log(' ')
+printSquare(2);
+printSquare(5);
+console.log(' ')
