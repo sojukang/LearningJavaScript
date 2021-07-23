f62ff933328cbc2c2ddc7b432ee7f61c8410e7b8
sojukang <tmdwn_1112@naver.com>
2021-07-23 18:29:09 +0900

    Add myVoca in object.js

---
+++ object.js
@@ -0,0 +1,15 @@
+// myVoca object for learning object
+let myVoca = {
+    function: '함수',
+    variable: '변수',
+    constant: '상수',
+    local: '지역의',
+    global: '전반적인'
+};
+
+// test code
+console.log(myVoca);
+console.log(myVoca.local);
+console.log(myVoca.constant);
+console.log(myVoca.function);
+
