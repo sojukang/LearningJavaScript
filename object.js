cf78387fd874be6ece4efbceaf9c0755c462168d
sojukang <tmdwn_1112@naver.com>
2021-07-24 18:04:06 +0900

    Add passChecker in object.js

--- object.js
+++ object.js
@@ -83,3 +83,39 @@ console.log(myVoca);
 
 // printVoca메소드 테스트 코드
 myVoca.printVoca('property');
+
+// .... //
+
+// passChecker function for learning for ... in statement 
+let hyesoonScore = {
+    '데이터 모델링의 이해': 10,
+    '데이터 모델과 성능': 8,
+    'SQL 기본': 22,
+    'SQL 활용': 18,
+    'SQL 최적화 기본 원리': 20,
+};
+
+let minsoonScore = {
+    '데이터 모델링의 이해': 14,
+    '데이터 모델과 성능': 8,
+    'SQL 기본': 12,
+    'SQL 활용': 4,
+    'SQL 최적화 기본 원리': 16,
+};
+
+function passChecker(scoreObject) {
+    let totalScore = 0;
+    for (subject in scoreObject) {
+        totalScore += scoreObject[subject];
+    }
+    
+    if (totalScore >= 60) {
+        console.log('축하합니다! 합격입니다!');
+    } else {
+        console.log('아쉽지만 불합격입니다..');
+    }
+}
+
+// test code
+passChecker(hyesoonScore);
+passChecker(minsoonScore);
