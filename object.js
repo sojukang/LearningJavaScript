b107acf2187cd2d337c17c75a89f493c042755a8
sojukang <tmdwn_1112@naver.com>
2021-07-24 18:21:42 +0900

    Add workDayCalc in object.js

--- object.js
+++ object.js
@@ -119,3 +119,22 @@ function passChecker(scoreObject) {
 // test code
 passChecker(hyesoonScore);
 passChecker(minsoonScore);
+
+
+// .... //
+
+// workDayCalc function for learning Date object
+let today = new Date(2112, 8, 24);
+let jaeSangStart = new Date(2109, 7, 1);
+
+function workDayCalc(startDate) {
+    // 여기에 코드를 작성해 주세요.
+    let timeDiff = today.getTime() - startDate.getTime();
+    let dayDiff = timeDiff / 1000 / 60 / 60 / 24;
+
+    console.log(`오늘은 입사한 지 ${dayDiff + 1}일째 되는 날 입니다.`);
+}
+
+workDayCalc(jaeSangStart);
+
+
