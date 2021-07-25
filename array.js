8fce7955f977eb6ca688c1dc552d9043b6e39797
sojukang <tmdwn_1112@naver.com>
2021-07-25 19:48:32 +0900

    Add multi-dimentional array example in array.js

--- array.js
+++ array.js
@@ -96,3 +96,31 @@ for (let name of votes) {
 console.log(voteCounter);
 
 // .... //
+
+// muti-dimensional array example
+
+let groups = [
+    ['영준', '캡틴'],
+    ['태순', '우재'],
+    ['재훈', '지웅'],
+    ['윤형', '동욱'],
+    ['규식', '소원'],
+];
+
+let teams = [
+    [],
+    [],
+];
+
+// 여기에 코드를 작성해 주세요.
+for (let i in groups) {
+    for (let j in groups[i]) {
+        teams[j][i] = groups[i][j];
+    } 
+}
+
+
+// 테스트 코드
+console.log(teams[0]);
+console.log(teams[1]);
+
